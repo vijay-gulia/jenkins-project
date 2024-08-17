7dd24353315e3a5af6f3c34e214133a0ec30481b
Vijay Gulia <vijaygulia143341@gmail.com>
2024-08-17 15:31:18 +0530

    Initial commits jenkins project

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world from java jenins project ");
+		System.out.println("Hello world from java jenkins project It is good practice");
 		System.out.println("Jenkins java test project compile java file ");
 
 		
