a384f0c2c485535f581371d5712cc6ff145811a7
Vijay Gulia <vijaygulia143341@gmail.com>
2024-08-17 13:52:17 +0530

    Initial commits jenkins project

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world from java jenins project ");
+		System.out.println("Jenkins java test project compile java file ");
+
+		
+	}
+
+}
